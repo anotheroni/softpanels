28cf826674289e8e6c05a96bd56d0dc01aaf5262
Oskar Nilsson <n.oskar@gmail.com>
2013-11-11 21:38:09 +0100

    A bit of cleanup

--- public/panel02.js
+++ public/panel02.js
@@ -37,21 +37,22 @@ var doconfig = jsonpanel['config']['do'];
 var diconfig = jsonpanel['config']['di'];
 var aoconfig = jsonpanel['config']['ao'];
 var elements = {};
-var ao01;
-var ao02;
-var ao03;
-var socket;
-var connected=0;
-var valvpos=0;
-var pumpflow=0;
-var level=0.0;
 var animating = {};
 var cleartimers = {};
+
+var socket;
+var connected=false;
+
 var subdrawingsloaded = false;
 var panelloaded = false;
 var panel;
 var subdrawings;
-// snap.svg
+
+var valvpos=0;
+var pumpflow=0;
+var level=0.0;
+
+// Load base drawing and subdrawings
 var s = Snap("#svg");
 Snap.load('subdrawings01.svg', function(o) {
   subdrawings = o;
@@ -67,10 +68,10 @@ Snap.load("panel02.svg", function(o) {
 // Socket.io 
 socket = io.connect();
 socket.on('connect', function () {
-  connected=1;
+  connected=true;
 });
 socket.on('disconnect', function() {
-  connected=0;
+  connected=false;
 });
 socket.on('message', function(data) {
   var obj=JSON.parse(data);
@@ -78,7 +79,7 @@ socket.on('message', function(data) {
   pumpflow=obj.pumpflow;
 });
 
-
+// Button animation
 function show(key) {
     if (animating[key]) return;
     clearTimeout(cleartimers[key]);
@@ -98,10 +99,11 @@ function show(key) {
 
 // Callback for onclick on buttons
 function pbclick() {
-//  show(this.node.id);
+  show(this.node.id);
   socket.send(this.node.id);
 }
 
+// Set fill color of element
 function setfill(key, color) {
   try {
     elements[key][0].attr({fill:color});
@@ -110,18 +112,23 @@ function setfill(key, color) {
   }
 }
 
+// Build panel from base drawing plus subdrawings
 function onDrawingLoaded(d){
-  ao01 = d.select("#needle01");
-  ao02 = d.select("#needle02");
-  ao03 = d.select("#needle03");
   s.append(d);
   var sd_meter01 = subdrawings.select("#meter01");
+  var sd_needle01 = subdrawings.select("#needle01");
   for (i=0; i<aoconfig.length; i++) {
     var key = aoconfig[i]['id'];
+    // Add meter
     var obj = sd_meter01.clone();
     // [18:22:38.021] NS_ERROR_FAILURE: Component returned failure code: 0x80004005 (NS_ERROR_FAILURE) [nsIDOMSVGLocatable.getBBox] @ http://localhost:8080/snap.svg.js:5371
-    //obj.attr({id: "#"+key, transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
+    //obj.attr({id: "#meter"+key, transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
+    obj.attr({id: "#meter"+key});
+    s.append(obj);
+    // Add needle
+    var obj = sd_needle01.clone();
     obj.attr({id: "#"+key});
+    //obj.attr({id: "#"+key, transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
     s.append(obj);
     elements[key] = [obj];
   }
@@ -148,9 +155,10 @@ function onDrawingLoaded(d){
   requestAnimationFrame(step);
 }
 
+// Draw "loop"
 function step(timestamp) {
-  if (connected==0) { setfill('lp00', '#ff0000'); }
-  else { setfill('lp00', '#00ff00'); }
+  if (connected) { setfill('lp00', '#00ff00'); }
+  else { setfill('lp00', '#ff0000'); }
   if (pumpflow==0) {
     setfill('lp01', '#00ff00');
     setfill('lp02', '#dddb36');
@@ -169,25 +177,19 @@ function step(timestamp) {
     setfill('lp05', '#fffc28');
   }
 
+  // "simulation"
   level = level + pumpflow*0.1 - valvpos*0.3;
   level = Math.max(0.0,level)
   
-  var m01 = new Snap.Matrix();
-  m01.translate(146,510);
-  m01.rotate(pumpflow*18,15,15);
-  m01.scale(0.6,0.6,15,15);
-  ao01.transform(m01);
-  var m02 = new Snap.Matrix();
-  m02.translate(396,510);
-  m02.rotate(pumpflow*18,15,15);
-  m02.scale(0.6,0.6,15,15);
-  ao02.transform(m02);
-  var m03 = new Snap.Matrix();
-  //m03.translate(595,120);
-  //m03.rotate(level,15,15);
-  m03.rotate(level,610,135);
-  //m03.scale(0.6,0.6,15,15);
-  ao03.transform(m03);
+  //var m01 = new Snap.Matrix();
+  //m01.rotate(pumpflow*18,15,15);
+  //ao01.transform(m01);
+  //var m02 = new Snap.Matrix();
+  //m02.rotate(pumpflow*18,15,15);
+  //ao02.transform(m02);
+  //var m03 = new Snap.Matrix();
+  //m03.rotate(level,610,135);
+  //ao03.transform(m03);
 
   requestAnimationFrame(step);
 }
