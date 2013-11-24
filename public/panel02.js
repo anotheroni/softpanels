2d065a70bc3ab04c279a31c4004c39a7d014d45c
Oskar Nilsson <n.oskar@gmail.com>
2013-11-24 16:57:48 +0100

    Back to a working version. Using subdrawgins for all dynamic objects.

--- public/panel02.js
+++ public/panel02.js
@@ -9,7 +9,7 @@ window.onload = function() {
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-// JSON definition of panel
+// JSON definition of panel subdrawings
 var jsonpanel = { "config": {
     "do": [
         { "id":"lp00", "x":10, "y":10 },
@@ -19,16 +19,16 @@ var jsonpanel = { "config": {
         { "id":"lp05", "x":550, "y":700 },
     ],
     "di": [
-        { "id":"pb01", "x":80, "y":170 },
-        { "id":"pb02", "x":130, "y":170 },
-        { "id":"pb03", "x":180, "y":170 },
-        { "id":"pb04", "x":500, "y":650 },
-        { "id":"pb05", "x":550, "y":650 },
+        { "id":"pb01", "x":70, "y":155 },
+        { "id":"pb02", "x":120, "y":155 },
+        { "id":"pb03", "x":170, "y":155 },
+        { "id":"pb04", "x":490, "y":635 },
+        { "id":"pb05", "x":540, "y":635 },
     ],
     "ao": [
-        { "id":"ao01", "x":20, "y":300 },
-        { "id":"ao02", "x":200, "y":300 },
-        { "id":"ao03", "x":500, "y":10 },
+        { "id":"ao01", "x":70, "y":400 },
+        { "id":"ao02", "x":320, "y":400 },
+        { "id":"ao03", "x":515, "y":60 },
     ]
 }};
 
@@ -49,10 +49,10 @@ var panel;
 var subdrawings;
 
 var valvpos=0;
-var pumpflow=0;
-var level=0.0;
+var oldpumpflow=pumpflow=0;
+var oldlevel=level=0.0;
 
-// Load base drawing and subdrawings
+// Load base drawing and subdrawings, draw when both are loaded.
 var s = Snap("#svg");
 Snap.load('subdrawings01.svg', function(o) {
   subdrawings = o;
@@ -84,17 +84,22 @@ function show(key) {
     if (animating[key]) return;
     clearTimeout(cleartimers[key]);
     animating[key] = true;
-    var element = elements[key],
-        x = element[0].getBBox().x,
-        offset = x/2 + 25;
+    var element = elements[key];
     element[0].attr({
-        transform: "translate("+offset+",80) scale(0.5, 0.5)"
+        transform: "translate("+element[0].mx+","+element[0].my+") scale(0.8, 0.8, 50, 50)"
     });
     setTimeout(function() {
-        element[0].animate({opacity:1,transform:""}, 500, mina.elastic);
-    }, 50);
-    //element[1].animate({opacity:0.25}, 400);
-    setTimeout(function(){animating[key] = false}, 550);
+      element[0].attr({
+        transform: "translate("+element[0].mx+","+element[0].my+") scale(1.0, 1.0, 50, 50)"
+      });
+      // Using animate will destroy the transform and all elements end up
+      // in the top left corner.
+      //element[0].animate({
+      //  transform: "scale(1.0, 1.0, 50, 50)"
+      //},500);
+        //element[0].animate({opacity:1,transform:"translate("+element[0].mx+","+element[0].my+")"}, 500, mina.elastic);
+      animating[key] = false
+    }, 300);
 }
 
 // Callback for onclick on buttons
@@ -121,32 +126,41 @@ function onDrawingLoaded(d){
     var key = aoconfig[i]['id'];
     // Add meter
     var obj = sd_meter01.clone();
+    // Firefox error
     // [18:22:38.021] NS_ERROR_FAILURE: Component returned failure code: 0x80004005 (NS_ERROR_FAILURE) [nsIDOMSVGLocatable.getBBox] @ http://localhost:8080/snap.svg.js:5371
-    //obj.attr({id: "#meter"+key, transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
-    obj.attr({id: "#meter"+key});
+    obj.node.id = "#meter" + key;
+    obj.attr({transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
     s.append(obj);
     // Add needle
     var obj = sd_needle01.clone();
-    obj.attr({id: "#"+key});
-    //obj.attr({id: "#"+key, transform: "translate("+aoconfig[i]['x']+","+aoconfig[i]['y']+")"});
+    obj.mx = aoconfig[i]['x']+80;
+    obj.my = aoconfig[i]['y']+80;
+    obj.node.id = key;
+    obj.attr({transform: "translate("+obj.mx+","+obj.my+")"});
     s.append(obj);
-    elements[key] = [obj];
+    var matrix = new Snap.Matrix();
+    matrix.translate(obj.mx, obj.my);
+    elements[key] = [obj, matrix];
   }
   var sd_lp01 = subdrawings.select("#lp01");
   for (i=0; i<doconfig.length; i++) {
     var key = doconfig[i]['id'];
     var obj = sd_lp01.clone();
-    obj.attr({id: "#"+key, transform: "translate("+doconfig[i]['x']+","+doconfig[i]['y']+")"});
+    var front = obj.select(".front");
+    obj.node.id = "g" + key; 
+    front.node.id = key; 
+    obj.attr({transform: "translate("+doconfig[i]['x']+","+doconfig[i]['y']+")"});
     s.append(obj);
-    elements[key] = [obj];
+    elements[key] = [front];
   }
   var sd_bp01 = subdrawings.select("#pb01");
   for (i=0; i<diconfig.length; i++) {
     var key = diconfig[i]['id'];
     var obj = sd_bp01.clone();
-    obj.attr({id: "#"+key});
-    //obj.attr({transform: "translate(0,0)"});
-    //, transform: "translate("+diconfig[i]['x']+","+diconfig[i]['y']+")"});
+    obj.mx = diconfig[i]['x'];
+    obj.my = diconfig[i]['y'];
+    obj.node.id = key;
+    obj.attr({transform: "translate("+diconfig[i]['x']+","+diconfig[i]['y']+")"});
     obj.click(pbclick);
     s.append(obj);
     elements[key] = [obj];
@@ -181,15 +195,18 @@ function step(timestamp) {
   level = level + pumpflow*0.1 - valvpos*0.3;
   level = Math.max(0.0,level)
   
-  //var m01 = new Snap.Matrix();
-  //m01.rotate(pumpflow*18,15,15);
-  //ao01.transform(m01);
-  //var m02 = new Snap.Matrix();
-  //m02.rotate(pumpflow*18,15,15);
-  //ao02.transform(m02);
-  //var m03 = new Snap.Matrix();
-  //m03.rotate(level,610,135);
-  //ao03.transform(m03);
+  if (oldpumpflow != pumpflow) {
+    elements["ao01"][1].rotate((pumpflow-oldpumpflow)*30,8.5,8.5);
+    elements["ao01"][0].transform(elements["ao01"][1]);
+    elements["ao02"][1].rotate((pumpflow-oldpumpflow)*30,8.5,8.5);
+    elements["ao02"][0].transform(elements["ao02"][1]);
+    oldpumpflow=pumpflow;
+  }
+  if (oldlevel != level) {
+    elements["ao03"][1].rotate((level-oldlevel),8.5,8.5);
+    elements["ao03"][0].transform(elements["ao03"][1]);
+    oldlevel = level;
+  }
 
   requestAnimationFrame(step);
 }
